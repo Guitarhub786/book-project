44a84d4e603a3fe08932edd2f1b90567de1c73db
Guitarhub786 <codewithdrawal@gmail.com>
2020-11-05 02:20:24 +0000

    add state/props to Books and SearchArea

--- src/Books.js
+++ src/Books.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import SearchArea from './SearchArea'
 
 class Books extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      books: [],
+      searchField: ""
+    }
+  }
+
+  handleSearch = (e) => {
+    console.log(e.target.value)
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     return (
       <div>
-        <SearchArea />
+        <SearchArea handleSearch={this.handleSearch} />
+        <h3>{this.state.searchField}</h3>
       </div>
     );
   }
